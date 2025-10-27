ccc52b77873492db5e179257799dce6ced2018a4
lijanahmmed <lijanahmmed@gmail.com>
2025-10-28 00:31:11 +0600

    added card scetion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,7 +14,6 @@ const Card = ({ issue, data, setData }) => {
 
     const restData = data.filter((elem) => elem.ticketId != issue.ticketId);
 
-    // console.log({ currentData, restData });
     setData([currentData, ...restData]);
   };
 
